d3587103569ebaf2816eaaa212cfd8209ef1fd25
Nick Hill <nickhill2380@gmail.com>
2021-03-02 12:12:58 -0500

    tested and fixed thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -28,7 +28,7 @@ const thoughtController = {
     Thought.create(body)
       .then(({ _id }) => {
         return User.findOneAndUpdate(
-          { _id: params.userId },
+          { _id: body.userId },
           { $push: { thoughts: _id } },
           { new: true }
         );
@@ -58,24 +58,8 @@ const thoughtController = {
 
    // remove thought
    removeThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.thoughtId })
-      .then(deletedThought => {
-        if (!deletedThought) {
-          return res.status(404).json({ message: 'No thought with this id!' });
-        }
-        return User.findOneAndUpdate(
-          { _id: params.userId },
-          { $pull: { thoughts: params.thoughtId } },
-          { new: true }
-        );
-      })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
       .catch(err => res.json(err));
   },
   
