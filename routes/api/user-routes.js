ebef1001036eb1b58d27d76c9a8dcddfc4f82017
Nick Hill <nickhill2380@gmail.com>
2021-03-02 11:25:53 -0500

    added thought and user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    deleteUser,
+    updateUser
+} = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .delete(deleteUser)
+    .put(updateUser);
